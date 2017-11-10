4da6fc5941f6b01b828ddddb2e0eafe98187dc99
Pablo Murcia Odriozo <pmurciao14@gmail.com>
2017-11-10 12:17:19 +0100

    KeepScreenOn & improve isLoading

--- app/src/main/java/pablomurciaodriozola/gorlizwebcam/MainActivity.kt
+++ app/src/main/java/pablomurciaodriozola/gorlizwebcam/MainActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import com.squareup.picasso.Callback
 import com.squareup.picasso.MemoryPolicy
 import com.squareup.picasso.NetworkPolicy
 import com.squareup.picasso.Picasso
@@ -11,27 +12,28 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val delay = 100L
+    private val delay = 800L
 
     private var handler = Handler()
     private var isLoading = false
     private var runnable = Runnable({})
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
     override fun onStart() {
+        isLoading = false
         handler.postDelayed(object : Runnable {
             override fun run() {
                 loadImage()
                 runnable = this
-                handler.postDelayed(this, 1000)
+                handler.postDelayed(this, delay)
             }
         }, delay)
 
         super.onStart()
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 
     private fun loadImage(){
@@ -41,13 +43,21 @@ class MainActivity : AppCompatActivity() {
                     .memoryPolicy(MemoryPolicy.NO_CACHE)
                     .networkPolicy(NetworkPolicy.NO_CACHE)
                     .noPlaceholder()
-                    .into(ivMain)
-            isLoading = false
+                    .into(ivMain, object : Callback {
+                        override fun onSuccess() {
+                            isLoading = false
+                        }
+
+                        override fun onError() {
+                            isLoading = false
+                        }
+                    })
         }
     }
 
     override fun onPause() {
         handler.removeCallbacks(runnable)
+        isLoading = false;
         super.onPause()
     }
 }
