47c8f803c3708467aad7594b298b23a298e1f445
Pablo Murcia Odriozo <pmurciao14@gmail.com>
2017-11-10 11:59:37 +0100

    Load Image every second & stop request onPause

--- app/src/main/java/pablomurciaodriozola/gorlizwebcam/MainActivity.kt
+++ app/src/main/java/pablomurciaodriozola/gorlizwebcam/MainActivity.kt
@@ -2,6 +2,8 @@ package pablomurciaodriozola.gorlizwebcam
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import com.squareup.picasso.MemoryPolicy
 import com.squareup.picasso.NetworkPolicy
 import com.squareup.picasso.Picasso
@@ -9,14 +11,43 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val delay = 100L
+
+    private var handler = Handler()
+    private var isLoading = false
+    private var runnable = Runnable({})
+
+    override fun onStart() {
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                loadImage()
+                runnable = this
+                handler.postDelayed(this, 1000)
+            }
+        }, delay)
+
+        super.onStart()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        Picasso.with(this).load(getString(R.string.imageUrl))
-                .memoryPolicy(MemoryPolicy.NO_CACHE)
-                .networkPolicy(NetworkPolicy.NO_CACHE)
-                .noPlaceholder()
-                .into(ivMain)
+    private fun loadImage(){
+        if (!isLoading){
+            isLoading = true
+            Picasso.with(this).load(getString(R.string.imageUrl))
+                    .memoryPolicy(MemoryPolicy.NO_CACHE)
+                    .networkPolicy(NetworkPolicy.NO_CACHE)
+                    .noPlaceholder()
+                    .into(ivMain)
+            isLoading = false
+        }
+    }
+
+    override fun onPause() {
+        handler.removeCallbacks(runnable)
+        super.onPause()
     }
 }
